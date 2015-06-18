791866964525b0e7837437c36bcc93e6334ff641
Mejdi Ourir <m.ourir@ivsweb.com>
2015-06-18 18:19:45 +0200

    WIN git checkout dev

--- sample.js
+++ sample.js
@@ -1,11 +1,25 @@
-var Omx = require('.');
+var Omx = require('./lib/omx');
 
-var playlist = ['/var/cache/media/4e/a381/4ea381b59889940b18cf0b966b144c39', '/var/cache/media/3a/ea86/3aea86ce941f5bb1ecb1868b82990bfc'];
+// var playlist = ['/var/cache/activscreen/media/34/23fb/3423fbbb8d87f503655cddaeb07c10a2' , '/var/cache/activscreen/media/34/23fb/3423fbbb8d87f503655cddaeb07c10a2'];
+
+
+
+var playlist = [ '/var/cache/activscreen/media/34/23fb/3423fbbb8d87f503655cddaeb07c10a2',
+  '/var/cache/activscreen/media/ff/6c5a/ff6c5a2e92bc1db711843c65f8c478b5',
+  '/var/cache/activscreen/media/3a/ea86/3aea86ce941f5bb1ecb1868b82990bfc',
+  '/var/cache/activscreen/media/4e/a381/4ea381b59889940b18cf0b966b144c39',
+  '/var/cache/activscreen/media/d6/d933/d6d9331638602d5c6ff674e4900975c8' ]
 
 var player = new Omx();
 
-player.play(playlist, {loop : true});
+player.play(playlist , {loop:true});
 
+
+setTimeout(function(){
+player.playonce('/var/cache/activscreen/media/4e/a381/4ea381b59889940b18cf0b966b144c39')}, 5000)
+
+
+/*
   //add item to playlist
 player.queue(playlist[1], function(){
   player.next();
@@ -21,3 +35,4 @@ setTimeout(function(){
 
 
 setTimeout(player.stop, 10 * 1000);
+*/
