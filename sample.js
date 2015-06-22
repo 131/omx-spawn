6400fbe3f9cbb7731c8b0c4cabd6d49518a29082
Mejdi Ourir <m.ourir@ivsweb.com>
2015-06-22 18:04:59 +0200

    rename stop in kill

--- sample.js
+++ sample.js
@@ -14,12 +14,11 @@ var player = new Omx();
 
 player.play(playlist , {loop:true});
 
+player.kill() ;
 
-/*
 setTimeout(function(){
 player.playonce('/var/cache/activscreen/media/3a/ea86/3aea86ce941f5bb1ecb1868b82990bfc')}, 5000)
 
-*/
 
 //---------------------//
 /*
